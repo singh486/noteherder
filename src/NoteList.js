caec0a2f59478ba0e5928d22f84eb1782d790143
Jasmine <singh486@purdue.edu>
2018-05-22 22:21:17 -0400

    Map hardcoded notes in the NoteList

--- src/NoteList.js
+++ src/NoteList.js
@@ -1,11 +1,89 @@
 import React from 'react'
+import './NoteList.css'
 
-const NoteList = () => {
-    return (
-        <div className="NoteList">
-            NoteList
-        </div>
-    )
+//const NoteList = () => 
+class NoteList extends React.Component{
+    constructor(props) {
+        super(props)
+        this.state = {
+            notes:[
+                    {title: "Kohlrabi welsh", content:"Veggies es bonus vobis, proinde vos postulo essum magis kohlrabi welsh onion daikon amaranth tatsoi tomatillo melon azuki bean garlic."},
+                    {title: "Dandelion cucumber", content:"Gumbo beet greens corn soko endive gumbo gourd. Parsley shallot courgette tatsoi pea sprouts fava bean collard greens dandelion okra wakame tomato. Dandelion cucumber earthnut pea peanut soko zucchini."},
+                    {title: "Prairie turnip", content: "Nori grape silver beet broccoli kombu beet greens fava bean potato quandong celery. Bunya nuts black-eyed pea prairie turnip leek lentil turnip greens parsnip."}
+            ]
+        }
+    }
+
+    
+    render(){
+        console.log(this.state.notes)
+        return (
+            <div className="NoteList">
+              <h3>Notes</h3>
+              <ul id="notes">
+                {/* <a class="active">
+                  <li>
+                    <div class="note">
+                      <div class="note-title">
+                        Kohlrabi welsh
+                      </div>
+                      <div class="note-body">
+                        <p>
+                          Veggies es bonus vobis, proinde vos postulo essum magis kohlrabi welsh onion daikon amaranth tatsoi tomatillo melon azuki bean garlic.
+                        </p>
+                      </div>
+                    </div>
+                  </li>
+                </a>
+                <a>
+                  <li>
+                    <div class="note">
+                      <div class="note-title">
+                        Dandelion cucumber
+                      </div>
+                      <div class="note-body">
+                        <p>
+                          Gumbo beet greens corn soko endive gumbo gourd. Parsley shallot courgette tatsoi pea sprouts fava bean collard greens dandelion okra wakame tomato. Dandelion cucumber earthnut pea peanut soko zucchini.
+                        </p>
+                      </div>
+                    </div>
+                  </li>
+                </a>
+                <a>
+                  <li>
+                    <div class="note">
+                      <div class="note-title">
+                        Prairie turnip
+                      </div>
+                      <div class="note-body">
+                        <p>
+                          Nori grape silver beet broccoli kombu beet greens fava bean potato quandong celery. Bunya nuts black-eyed pea prairie turnip leek lentil turnip greens parsnip.
+                        </p>
+                      </div>
+                    </div>
+                  </li>
+                </a> */}
+
+                {this.state.notes.map(note =>
+                <a key={note.title}>
+                  <li>
+                    <div className="note">
+                      <div className="note-title">
+                        {note.title}
+                      </div>
+                      <div className="note-body">
+                        <p>
+                            {note.content}
+                        </p>
+                      </div>
+                    </div>
+                  </li>
+                </a>
+                )}
+              </ul>
+            </div>
+        )
+    }
 }
 
 export default NoteList
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,25 +1,133 @@
 import React from 'react'
+//import ReactDOM from 'react-dom'
 
 import quill from './quill.svg'
 import newIcon from './new.png'
 import newHover from './new-hover.png'
+
+
+
 const Sidebar = () => {
+
+    // this.state = {
+    //     hover: false
+    // }
+    //this.state.hover = false
+    var hover
+    var style
+
+   function renderStyle(){
+        if(hover){
+            return styles.imgOutline
+        }else{
+            console.log("render")
+            return styles.img
+        }
+    }
     return(
-        <nav className="Sidebar">
-          <div className="logo">
-            <img src={quill} alt="Noteherder"/>
+        <nav 
+            className="Sidebar"
+            style = {styles.sidebar}
+        >
+          <div 
+            className="logo"
+            style={styles.logo}
+          >
+            <img 
+                src={quill} 
+                alt="Noteherder"
+                style={styles.logoImg}
+            />
           </div>
-          <a className="new-note" href="/notes">
-            <img src={newHover} alt="New note"/>
-            <img className="outline" src={newIcon} alt="New note"/>
+            <a 
+            className="new-note" 
+            href="/notes"
+            style={styles.newNote}
+            >
+            <img 
+                src={newHover} 
+                alt="New note"
+                style={styles.img}
+            />
+            <img 
+                className="outline" 
+                src={newIcon} 
+                alt="New note"
+                onMouseOver={()=>{
+                    hover = true
+                    style = styles.img
+                    console.log(hover)
+                }}
+                onMouseLeave={()=>{
+                    hover = false
+                    style = styles.imgOutline
+                    console.log(hover)
+                }}
+                //hover ? styles.imgOutline : styles.img}
+                style={renderStyle()}
+
+            />
           </a>
-          <div className="SignOut">
-            <button>
-              <i className="fa fa-sign-out"></i>
+          <div 
+            className="SignOut"
+            style={styles.signOut}
+          >
+            <button style={styles.signOutButton}>
+              <i style={styles.signOutIfa} className="fa fa-sign-out"></i>
             </button>
           </div>
         </nav>
-    )
+    )}
+
+
+const styles = {
+    sidebar: {
+        width: '6rem',
+        backgroundColor: '#f3f3f3',
+        padding: '.5rem 0',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    logo: {
+        fontFamily: '"Fauna One"',
+        color: '#666',
+        fontSize: '3rem',
+    },
+    logoImg: {
+        width: '3rem',
+        paddingLeft: '.4rem',
+    },
+    newNote: {
+        marginTop: '2rem',
+        position: 'relative',
+        width: '40px',
+    },
+    button: {
+        backgroundColor: 'transparent',
+        border: '0',
+        color: '#008BF8',
+        cursor: 'pointer',
+    },
+    img: {
+        position: 'absolute',
+        left: '0',
+        width: '100%',
+        transition: 'opacity .25s ease-in-out',
+    },
+    imgOutline: {
+        opacity: '0',
+    },
+    signOut: {
+        position: 'absolute',
+        bottom: '1rem',
+    },
+    signOutButton: {
+        outline: 'none',
+    },
+    signOutIfa: {
+        fontSize: '2rem',
+    }   
 }
 
 export default Sidebar
