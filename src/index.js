f196128f01f1509f716945ddab08b536c34363b5
Jasmine <singh486@purdue.edu>
2018-05-30 15:47:51 -0400

    Use HashRouter for Github Pages instead of BrowserRouter

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { HashRouter as Router, Route } from 'react-router-dom'
 
 import './index.css'
 import App from './App'
